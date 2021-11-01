c5f9695afa41c3876dc84b3b9eb8da37711f6d84
Jônatas Henrique Jordão de Oliveira <jonatas.oliveira59@etec.sp.gov.br>
2021-11-01 19:00:53 -0300

    01/11/2021
    november forms

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-underscore-dangle */
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public async get(key: string) {
+   return await this._storage?.get(key);
+  }
+
+  public async delete(key: string) {
+    return await this._storage.remove(key);
+  }
+
+}
